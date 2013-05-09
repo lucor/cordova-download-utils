c153b3a87e19ac424023966f71c031fe10249bb0
lucor <lu.corbo@gmail.com>
2013-05-09 14:10:37 +0200

    Added progress bar handling

--- cordovaDownloadUtil.js
+++ cordovaDownloadUtil.js
@@ -1,8 +1,32 @@
+/**
+ * cordovaDownloadUtils
+ *
+ * Cordova helper to download and eventually unzip external files
+ *
+ * Usage:
+ * cordovaDownloadUtil.url = "http://example.com/archive.zip";
+ * cordovaDownloadUtil.unzip = true; //if true unzip and remove the downloaded file.
+ * cordovaDownloadUtil.progressBar = object with a setValue method to handle progress bar
+ * cordovaDownloadUtil.extractCallback = function(obj){
+ *    //perform action after unzip with success (optional)
+ * }
+ * cordovaDownloadUtil.download()
+ *
+ *
+ * author Luca Corbo (@lucacorbo)
+ *
+ * Thanks to:
+ * - https://github.com/phonegap/phonegap-plugins/blob/master/iOS/ExtractZipFile/README.md
+ * - http://docs.phonegap.com/en/2.7.0/cordova_file_file.md.html
+ * - http://stackoverflow.com/a/9940951
+ *
+ */
+
 var cordovaDownloadUtil = {
     url: '',
     unzip: false,
     extractCallback: false,
-
+    progressBar: false,
     download: function () {
         window.requestFileSystem(
             LocalFileSystem.PERSISTENT, 0,
@@ -11,10 +35,16 @@ var cordovaDownloadUtil = {
                     ".tmp", {create: true, exclusive: false},
                     function (fileEntry) {
                         var rootPath = fileEntry.fullPath.replace(".tmp","");
+                        fileEntry.remove();
                         var url = cordovaDownloadUtil.url;
                         var fileName = url.split('/').pop();
                         var fileTransfer = new FileTransfer();
-                        fileEntry.remove();
+                        fileTransfer.onprogress = function(progressEvent) {
+                             if (typeof cordovaDownloadUtil.progressBar == "object") {
+                                var val = Math.floor(progressEvent.loaded / progressEvent.total * 100);
+                                cordovaDownloadUtil.progressBar.setValue(val);
+                             }
+                        }
                         fileTransfer.download(
                             url,
                             rootPath + fileName,
